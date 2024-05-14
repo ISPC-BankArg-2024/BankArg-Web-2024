c4760af0b7f9497d35fea85872371b6492742b45
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2024-05-13 20:20:35 -0700

    Update DB

--- Back/bankarg/bankarg/settings.py
+++ Back/bankarg/bankarg/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import dotenv
+# import dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -94,10 +94,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost",
-    "http://localhost:4200",
-]
+CORS_ORIGIN_WHITELIST = ["http://localhost", "http://localhost:4200"]
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "bankarg.urls"
@@ -131,7 +128,7 @@ DATABASES = {
         "USER": "root",
         "PASSWORD": "",
         "HOST": "localhost",
-        "PORT": "3308",
+        "PORT": "3306",
         "OPTIONS": {
             "sql_mode": "traditional",
         },
